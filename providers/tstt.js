28222462e0d4f6e9552a2831392bb4c546b90f05
julius1932 <30441147+julius1932@users.noreply.github.com>
2018-07-02 16:24:40 +0200

    Add files via upload

---
+++ providers/tstt.js
@@ -0,0 +1,10 @@
+var SpellChecker = require('simple-spellchecker');
+SpellChecker.getDictionary("en-US", function(err, dictionary) {
+	console.log(err);
+    if(!err) {
+        var misspelled = ! dictionary.spellCheck('maisonn');
+        if(misspelled) {
+            var suggestions = dictionary.getSuggestions('maisonn');
+        }
+    }
+});
