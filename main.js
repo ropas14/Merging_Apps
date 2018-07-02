09bea1dbf106b23f20fd7a079fac4c2ebefbd251
julius1932 <30441147+julius1932@users.noreply.github.com>
2018-07-02 15:16:24 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 var express = require("express");
 var app = express();
+
 var med_app = require("./app");
+var insu_app = require("./insurance/app");
+var prov_app = require("./provider/app");
 
 app.set('port', process.env.PORT || 3000);
+
 app.use('/medication', med_app);
+app.use('/insurance', insu_app);
+app.use('/provider', prov_app);
+
 app.get('/', function (req, res) {
   res.send("This is the '/' route in main_app");
 });
