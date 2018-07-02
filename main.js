0a53879fced931817090c67bd76a719009ba4228
julius1932 <30441147+julius1932@users.noreply.github.com>
2018-07-02 16:27:26 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ var app = express();
 
 var med_app = require("./medication/app");
 var insu_app = require("./insurance/app");
-var prov_app = require("./provider/app");
+var prov_app = require("./providers/app");
 
 app.set('port', process.env.PORT || 3000);
 
 app.use('/medication', med_app);
 app.use('/insurance', insu_app);
-app.use('/provider', prov_app);
+app.use('/providers', prov_app);
 
 app.get('/', function (req, res) {
   res.send("This is the '/' route in main_app");
